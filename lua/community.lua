c426cb7590a8ff9c78c0c1f0cee5216f3356652a
braunbearded <braunbearded1@gmail.com>
2024-04-15 23:58:30 +0200

    add community plugins

---
+++ lua/community.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.hop-nvim" },
+}
